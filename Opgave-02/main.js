57eafa0a38191a570b5a0ddd1ef62152a684751c
oliver <oliverbf@icloud.com>
2025-10-07 21:23:44 +0200

    Begyndt på opgave 4 & 6

--- Opgave-02/main.js
+++ Opgave-02/main.js
@@ -1,10 +1,12 @@
+// -------- Opgave 2 --------
+
 function getStartupName (){
     const firstWords = ["Tech", "Inno", "Smart", "Blue", "Next", "Bright", "Quantum", "Rapid", "Future", "Digital"];
     const secondWords = ["Vision", "Solutions", "Systems", "Works", "Dynamics", "Hub", "Network", "Innovations", "Labs", "Path"];
 
-    const wordOne = firstWords[Math.floor(Math.random() * firstWords.length)];
-    const wordTwo = secondWords[Math.floor(Math.random() * secondWords.length)];
+    const wordOne = firstWords[Math.floor(Math.random() * firstWords.length)]; // Kode fundet på Google
+    const wordTwo = secondWords[Math.floor(Math.random() * secondWords.length)]; // Kode fundet på Google
 
-    return wordOne + wordTwo
+    return wordOne + wordTwo // Returnerer et tilfældigt wordOne + wordTwo
 }
 console.log("Your startup name could be: " + getStartupName());
