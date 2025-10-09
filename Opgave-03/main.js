99463e40eec2a8b89157a96755d47305eab4eabf
oliver <oliverbf@icloud.com>
2025-10-09 14:48:43 +0200

    TIlføjet tekst til opgave 3

--- Opgave-03/main.js
+++ Opgave-03/main.js
@@ -1,20 +1,21 @@
-// -------- Opgave 3 --------
-// 3.1
+// Opgave 03 - Treasure Hunter
 
-let island = ['-', '-', '-', 'T', '-'];
-console.log("There are " + island.length +  " moves in the island")
+// 3.1 - Log out how many moves there are in the island
+
+const island = ['-', '-', '-', 'T', '-']; // Fra opgavebeskrivelse i Git
+console.log(`There are ${island.length} moves in the island`) // island.length tæller antal elementer i arrayet, som logges ud. Bruger template literals ${} til at blande variabler og string.
 
 // 3.2
-let treasureCount = 0;
+let treasureCount = 0; // Jeg laver en variabel der holder styr på antallet af fundne treasures. Den starter på 0;
 
-for (let i= 0; i < island.length; i++){
-    if (island[i] === "T"){
-        console.log("Treasure found at move " + (i + 1) + " :)");
-        treasureCount++;
+for (let i= 0; i < island.length; i++){ // Laver for loop, der gennemløber antal elementer i arrayet. i er indekset, som starter ved plads 0 i arrayet.
+    if (island[i] === "T"){ // tjekker (===) om der er fundet en treasure i arrayet
+        console.log("Treasure found at move " + (i + 1) + " :)"); //Logger if-betingelsen ud
+        treasureCount++; // Øger treasure count + 1, hver gang der findes en treasure
     } else {
-        console.log("No treasure was found :(");
+        console.log("No treasure was found :("); // Hvis if-betingelsen ikke opfyldes logges else-betingelsen ud i konsollen
     }
 }
 
 // 3.3
-console.log("There were " + treasureCount + " treasures in the island")
+console.log(`There were ${treasureCount} treasures in the island`) // Logger samlet antal treasures ud i konsollen.
